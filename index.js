2b9191e0094214dab7bf5a90817b5ad2a3a9b354
Jerry Kurian <jkurian770@gmail.com>
2018-02-13 09:16:01 -0500

    Completed recursive from from A to B

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  //sum(3,5)
+  //sum(3,3), sum(3,3) + 4, sum(3,3) + sum(3,4) + 5
+  if(fromN === toN) return toN;
+
+  return toN + sum(fromN, toN-1);
 }
 
 module.exports = sum;
